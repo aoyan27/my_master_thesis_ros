827fa68aacafd1ec7e9066633e6561f6b8d92a64
aoyan <ce62001@meiji.ac.jp>
2017-08-02 18:07:49 +0900

    added ssd with ros.py (modified chainer-SSD)

--- my_cv_bridge_tutorial/scripts/ssd/my_ssd_with_ros.py
+++ my_cv_bridge_tutorial/scripts/ssd/my_ssd_with_ros.py
@@ -9,6 +9,9 @@ import rospy
 import sys
 import cv2 as cv
 import numpy as np
+import skimage.io
+import skimage.draw
+from skimage.transform import resize
 
 from std_msgs.msg import String, Header, Int32MultiArray
 from sensor_msgs.msg import Image
@@ -16,18 +19,19 @@ from cv_bridge import CvBridge, CvBridgeError
 
 import chainer
 from chainer import serializers
-from chainer.cuda import to_gpu
-from utils.cpu_nms import cpu_nms as nms
-from utils.faster_rcnn import FasterRCNN
-from utils.vgg16 import VGG16Prev
+from chainer import cuda
 
-import copy
+from utils.prior import prior
+
+import utils.ssd_net
+import utils.draw
+
 import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--nms_thresh', type=float, default=0.3)
-parser.add_argument('--conf', type=float, default=0.8)
-parser.add_argument('--gpu', type=int, default=-1)
+import copy
+
+parser = argparse.ArgumentParser(description='detect object')
+parser.add_argument('--gpu', type=int, default=-1, help='Choose gpu(0) or cpu(-1) mode')
 args = parser.parse_args()
 xp = chainer.cuda.cupy if chainer.cuda.available and args.gpu >= 0 else np
 
@@ -41,7 +45,7 @@ class image_converter:
         #  self.image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.ImageCallback)
         self.image_sub = rospy.Subscriber("/zed/rgb/image_raw_color", Image, self.ImageCallback)
         
-        self.model_path = "/home/amsl/chainer-faster-rcnn/data/VGG16_faster_rcnn_final.model"
+        self.model_path = "/home/amsl/ros_catkin_ws/src/master_thesis/my_cv_bridge_tutorial/data/ssd/ssd.model"
 
         self.model = None
 
@@ -60,115 +64,78 @@ class image_converter:
         
         height, width, channels= cv_image.shape[:3]
         print "height : ", height, "\twidth : ", width, "\tchannels : ", channels
-        resize_image = cv.resize(cv_image, (width/2, height/2))
-
+        orig_image = cv.resize(cv_image, (width/2, height/2))
+        resize_image = cv.resize(cv_image, (300, 300))
+        
         #  orig_image = copy.deepcopy(cv_image)
-        orig_image = copy.deepcopy(resize_image)
+        #  orig_image = copy.deepcopy(resize_image)
         print('orig_image:', orig_image.shape)
-        img, im_scale = self.img_preprocessing(orig_image, PIXEL_MEANS)
-        img = np.expand_dims(img, axis=0)
-        print('img:', img.shape, im_scale)
-        if args.gpu >= 0:
-            img = to_gpu(img, device=args.gpu)
         
-        with chainer.using_config('enable_backprop', False):
-            img = chainer.Variable(img)
-        img_info = chainer.Variable(np.array([[img.shape[2], img.shape[2]]]))
-        cls_score, bbox_pred = self.model(img, img_info)
-        cls_score = cls_score.data
+        mean = np.array([104, 117, 123])
+        resize_image = cv.cvtColor(resize_image, cv.COLOR_BGR2RGB)
+        resize_image = resize_image.astype(np.float32)
 
-        if args.gpu >= 0:
-            cls_score = chainer.cuda.cupy.asnumpy(cls_score)
-            bbox_pred = chainer.cuda.cupy.asnumpy(bbox_pred)
-        result = self.draw_result(orig_image, im_scale, cls_score, bbox_pred, args.nms_thresh, args.conf)
+        img = resize_image - mean[::-1]
+        img = img.transpose(2, 0, 1)[::-1]
+
+        #  print "img : ", img
         
-        self.image_pub.publish(self.bridge.cv2_to_imgmsg(result, "bgr8"))
+        start = time.time()
+
+        x = None
+        if args.gpu >= 0:
+            img_gpu = cuda.to_gpu(np.array([img], dtype=np.float32))
+            x = chainer.Variable(img_gpu)
+        else:
+            img_cpu = np.array([img], dtype=np.float32)
+            x = chainer.Variable(img_cpu)
+
+        self.model(x, 1, 1, 1, 1)
+
+        prior = self.model.mbox_prior.astype(np.float32)
+        loc = cuda.to_cpu(self.model.mbox_loc.data[0])
+        conf = cuda.to_cpu(self.model.mbox_conf_softmax_reshape.data[0])
+
         elapsed_time = time.time() - start
         print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
-        #  cv.imshow("Origin Image", cv_image)
-        #  cv.imshow("Origin Image", resize_image)
+
+
+        cand = utils.draw.detect(prior, loc, conf)
+        #  utils.draw.draw(image, cand, args.name)
+        result, x1, y1, x2, y2, bbox_pub_flag = utils.draw.cv_draw(orig_image, cand)
+        print "cand : ", cand
+        if bbox_pub_flag:
+            print "x1 : ", x1, "\t y1 : ", y1, "\t x2 : ", x2, "\t y2 : ", y2
+
+            bbox_array = Int32MultiArray()
+            bbox_array.data = [x1, y1, x2, y2]
+            self.bbox_pub.publish(bbox_array)
+        
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(result, "bgr8"))
+
+
+        #  cv.imshow("Origin Image", orig_image)
         #  cv.imshow("Result Image", result)
 
-
     def get_model(self):
-        self.model = FasterRCNN(trunk_class=VGG16Prev)
-        self.model.rcnn_train = False
-        self.model.rpn_train = False
+        self.model = utils.ssd_net.SSD()
         serializers.load_npz(self.model_path, self.model)
 
-        if chainer.cuda.available and args.gpu >= 0:
+        if args.gpu >= 0:
+            print "--- GPU mode ---"
             self.model.to_gpu(args.gpu)
+        else:
+            print "--- CPU mode ---"
 
-        print "model load complete!!!"
-
-        #  return model
-
-    def img_preprocessing(self, orig_img, pixel_means, max_size=1000, scale=600):
-        img = orig_img.astype(np.float32, copy=True)
-        img -= pixel_means
-        im_size_min = np.min(img.shape[0:2])
-        im_size_max = np.max(img.shape[0:2])
-        im_scale = float(scale) / float(im_size_min)
-        if np.round(im_scale * im_size_max) > max_size:
-            im_scale = float(max_size) / float(im_size_max)
-        img = cv.resize(img, None, None, fx=im_scale, fy=im_scale,
-                        interpolation=cv.INTER_LINEAR)
-
-        return img.transpose([2, 0, 1]).astype(np.float32), im_scale
-
-    def draw_result(self, out, im_scale, clss, bbox, nms_thresh, conf):
-        CV_AA = 16
-        #  for cls_id in range(1, 21):
-            #  _cls = clss[:, cls_id][:, np.newaxis]
-            #  _bbx = bbox[:, cls_id * 4: (cls_id + 1) * 4]
-            #  dets = np.hstack((_bbx, _cls))
-            #  keep = nms(dets, nms_thresh)
-            #  dets = dets[keep, :]
-
-            #  inds = np.where(dets[:, -1] >= conf)[0]
-            #  #  print "inds : ", inds
-            #  for i in inds:
-                #  x1, y1, x2, y2 = map(int, dets[i, :4] / im_scale)
-                #  cv.rectangle(out, (x1, y1), (x2, y2), (0, 0, 255), 2, CV_AA)
-                #  ret, baseline = cv.getTextSize(CLASSES[cls_id], cv.FONT_HERSHEY_SIMPLEX, 0.8, 1)
-                #  cv.rectangle(out, (x1, y2 - ret[1] - baseline),(x1 + ret[0], y2), (0, 0, 255), -1)
-                #  cv.putText(out, CLASSES[cls_id], (x1, y2 - baseline), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, CV_AA)
-        cls_id = 15
-        _cls = clss[:, cls_id][:, np.newaxis]
-        _bbx = bbox[:, cls_id * 4: (cls_id + 1) * 4]
-        dets = np.hstack((_bbx, _cls))
-        keep = nms(dets, nms_thresh)
-        dets = dets[keep, :]
-
-        inds = np.where(dets[:, -1] >= conf)[0]
-        #  print "inds : ", inds
-        bbox_array = Int32MultiArray()
-        for i in inds:
-            x1, y1, x2, y2 = map(int, dets[i, :4] / im_scale)
-            print "x1 : ", x1, "\t y1 : ", y1, "\t x2 : ", x2, "\t y2 : ", y2
-            #  bbox_array = np.array([x1, y1, x2, y2], dtype=np.int32)
-            #  Int32MultiArray(bbox_array).data = [x1, y1, x2, y2]
-            #  bbox_array.layout.dim = [1]
-            bbox_array.data = [x1, y1, x2, y2]
-            print "bbox_array : ", bbox_array
-            cv.rectangle(out, (x1, y1), (x2, y2), (0, 0, 255), 2, CV_AA)
-            ret, baseline = cv.getTextSize(CLASSES[cls_id], cv.FONT_HERSHEY_SIMPLEX, 0.8, 1)
-            cv.rectangle(out, (x1, y2 - ret[1] - baseline),(x1 + ret[0], y2), (0, 0, 255), -1)
-            cv.putText(out, CLASSES[cls_id], (x1, y2 - baseline), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, CV_AA)
-
-            self.bbox_pub.publish(bbox_array)
-
-        return out
-
-
+        print "model load complete!!"
 
 
 def main(args):
     ic = image_converter()
     ic.get_model()
 
-    rospy.init_node("image_converter")
-
+    rospy.init_node("my_sdd_with_ros")
+    print "Here we go!!!!"
     #  cv.namedWindow("Origin Image")
     #  cv.namedWindow("Result Image")
     #  cv.startWindowThread()
