3be4a45dff1fec936bc6ce4d3fb6ac27bab3ce37
aoyan <ce62001@meiji.ac.jp>
2018-01-31 22:35:51 +0900

    added subscribe topic

--- infant_utils/src/velodyne_visual.cpp
+++ infant_utils/src/velodyne_visual.cpp
@@ -3,14 +3,29 @@
 #include <sensor_msgs/PointCloud2.h>
 
 
-ros::Publisher pub;
-sensor_msgs::PointCloud2 vel2;
+ros::Publisher pub1;
+ros::Publisher pub2;
+ros::Publisher pub3;
+
+void pVelPointCallback(const sensor_msgs::PointCloud2::Ptr& msg)
+{
+    msg->header.frame_id="velodyne2";
+    msg->header.stamp   =ros::Time::now(); 
+    pub1.publish(*msg);
+}
 
 void velPointCallback(const sensor_msgs::PointCloud2::Ptr& msg)
 {
     msg->header.frame_id="velodyne2";
     msg->header.stamp   =ros::Time::now(); 
-    pub.publish(*msg);
+    pub2.publish(*msg);
+}
+
+void velPointConsCallback(const sensor_msgs::PointCloud2::Ptr& msg)
+{
+    msg->header.frame_id="velodyne2";
+    msg->header.stamp   =ros::Time::now(); 
+    pub3.publish(*msg);
 }
 
 
@@ -18,8 +33,13 @@ int main(int argc, char** argv)
 {    
     ros::init(argc, argv, "velodyne2");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("/perfect_velodyne/normal", 10, velPointCallback);
-    pub = n.advertise<sensor_msgs::PointCloud2>("/velodyne2", 10);
+    ros::Subscriber sub1 = n.subscribe("/perfect_velodyne/normal", 10, pVelPointCallback);
+    ros::Subscriber sub2 = n.subscribe("/velodyne_points", 10, velPointCallback);
+    ros::Subscriber sub3 = n.subscribe("/velodyne_points/constraint", 10, velPointConsCallback);
+
+    pub1 = n.advertise<sensor_msgs::PointCloud2>("/velodyne2", 10);
+    pub2 = n.advertise<sensor_msgs::PointCloud2>("/velodyne2/normal", 10);
+    pub3 = n.advertise<sensor_msgs::PointCloud2>("/velodyne2/constraint", 10);
 
     ros::spin();
     return 0;
