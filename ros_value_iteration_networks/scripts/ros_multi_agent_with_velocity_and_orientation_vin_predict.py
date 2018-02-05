0569825abe0aad988a544d533206eeb9ebc40985
aoyan <ce62001@meiji.ac.jp>
2018-02-06 02:14:39 +0900

    bug fixed

--- ros_value_iteration_networks/scripts/ros_multi_agent_with_velocity_and_orientation_vin_predict.py
+++ ros_value_iteration_networks/scripts/ros_multi_agent_with_velocity_and_orientation_vin_predict.py
@@ -498,7 +498,7 @@ def main(model_path, gpu):
                            idg.other_position_data, idg.other_velocity_data, \
                            idg.other_orientation_data, idg.discreate_local_goal)
             print "agent.traj_state_list : ", agent.traj_state_list
-            agent.view_path(idg.input_data ,agent.traj_state_list)
+            #  agent.view_path(idg.input_data ,agent.traj_state_list)
 
             ros_next_state.data = np.asarray(agent.traj_state_list).reshape(-1)
             next_target_pub.publish(ros_next_state)
